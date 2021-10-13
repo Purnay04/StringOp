abd155573b908ade49121ffefe846162233e0fcf
Purnay Barge <bargeamanpurnay087@gmail.com>
2021-10-13 11:54:26 +0530

    some backend changes

--- stringop/src/main/java/com/example/stringop/controllers/StringOperationController.java
+++ stringop/src/main/java/com/example/stringop/controllers/StringOperationController.java
@@ -1,9 +1,13 @@
 package com.example.stringop.controllers;
 
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.stringop.pojo.InputString;
 
 @RestController
 public class StringOperationController {
@@ -19,5 +23,19 @@ public class StringOperationController {
 	public String DoLowercase(
 							  @RequestParam("string") String inputed_string) {
 		return inputed_string.toLowerCase();
+	}
+	
+	@CrossOrigin(origins = "http://localhost:4200")
+	@PostMapping(value= "/doUppercasePOST", consumes = "application/json", produces = "application/json")
+	public String DoUppercasePost(
+							  @RequestBody InputString string) {
+		return string.getInput_string().toUpperCase();
+	}
+	
+	@CrossOrigin(origins = "http://localhost:4200")
+	@PostMapping(value= "/doLowercasePOST", consumes = "application/json", produces = "application/json")
+	public String DoLowercasePost(
+			@RequestBody InputString string) {
+		return string.getInput_string().toLowerCase();
 	}
 }
---
+++ stringop/src/main/java/com/example/stringop/pojo/InputString.java
@@ -0,0 +1,13 @@
+package com.example.stringop.pojo;
+
+public class InputString {
+	String input_string;
+
+	public String getInput_string() {
+		return input_string;
+	}
+
+	public void setInput_string(String input_string) {
+		this.input_string = input_string;
+	}
+}
